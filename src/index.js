8d12e9efbb47d9191ab385b097a7f64b9162f8d2
siddhi Algude <algudesiddhi04@gmail.com>
2024-03-10 12:53:54 +0530

    added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express =require ('express');
 
 const app = express ();
 const apiRoutes = require('./routes/index');
+
 const bodyParser= require('body-parser');
 const {PORT} =require('./config/serverConfig');
-const bcrypt = require('bcrypt');
-const {User} = require('./models/index');
+
+// const bcrypt = require('bcrypt');
+// const {User} = require('./models/index');
+// const UserRepository = require('./repository/user-repository');
+ 
 const prepareAndStartServer = () =>{
 
   app.use(bodyParser.json());
@@ -14,10 +18,14 @@ const prepareAndStartServer = () =>{
  
   app.listen(PORT,async () => {
     console.log(`server started at ${PORT}`);
-    const incomingpassword = 'mochiio';
-    const user = await User.findByPk(5)
-    const response = bcrypt.compareSync(incomingpassword,user.password); 
-    console.log(response);
+
+    // const repo = new UserRepository();
+    // const response = await repo.getById(2);
+    // console.log(response);
+    // //const incomingpassword = 'mochii';
+    // const user = await User.findByPk(5)
+    // const response = bcrypt.compareSync(incomingpassword,user.password); 
+    // console.log(response);
   });
 }
 prepareAndStartServer();
